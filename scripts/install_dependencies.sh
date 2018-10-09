fc58bee12baa404664044c39323015dffc29686d
Joe DeGol <jomnipotent17@yahoo.com>
2018-10-09 00:10:42 -0500

    updated script for 18.04

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,11 @@
-# A script to install dependencies. I tested this with 18.04. I suspect it will work in 16.04 also.
-# If you run this with 14.04, you will need to uninstall cmake and build an updated version (as outlined)
-# in the README.md
+# A script to install dependencies. I tested this with 14.04, 16.04, and 18.04. I do not know if this
+# will work with the other versions, but if you follow the comments below, you can figure out what needs
+# to be done
 
 
 # prompt user
 echo "This script will install all the dependencies for MarkerSfM. It has been"
-echo "successfully tested it with Ubuntu 14.04 and 16.04"
+echo "successfully tested it with Ubuntu 14.04, 16.04, and 18.04"
 echo "Do you wish to continue?"
 select yn in "Yes" "No"; do
     case $yn in
@@ -14,11 +14,13 @@ select yn in "Yes" "No"; do
     esac
 done
 
+# get ubuntu version
+osversion=$( lsb_release -r | awk '{ print $2 }' | sed 's/[.]//' )
 
 # install common
 sudo apt-get update
 sudo apt-get install -y git build-essential pkg-config wget unzip
-sudo apt-get install -y libatlas-base-dev libboost-all-dev libboost-python-dev libsuitesparse-dev libeigen3-dev libgoogle-glog-dev 
+sudo apt-get install -y libatlas-base-dev libsuitesparse-dev libeigen3-dev libgoogle-glog-dev 
 sudo apt-get install -y python-dev python-numpy python-pip python-pyexiv2 python-pyproj python-scipy
 sudo pip install --upgrade pip
 sudo pip install exifread==2.1.2
@@ -30,12 +32,23 @@ sudo pip install python-dateutil==2.6.0
 sudo pip install PyYAML==3.12
 sudo pip install xmltodict==0.10.2
 
+# install boost. 1.54 for Ubuntu 14.04 and 1.58 for Ubuntu 16.04, 18.04
+if [ "$osversion" -eq "1804" ]; then
+    echo "deb http://archive.ubuntu.com/ubuntu/ xenial main universe" | sudo tee -a /etc/apt/sources.list
+    echo "deb http://archive.ubuntu.com/ubuntu/ xeniel-updates main universe" | sudo tee -a /etc/apt/sources.list
+    sudo apt-get update
+    sudo apt-get install -y aptitude
+    sudo aptitude install -y libboost1.58-all-dev
+    sudo aptitude install -y libboost-python1.58-dev
+else
+    libboost-all-dev libboost-python-dev
+fi
+
 # move to Downloads directory
 mkdir -p ~/Downloads
 cd ~/Downloads
 
-# cmake
-osversion=$( lsb_release -r | awk '{ print $2 }' | sed 's/[.]//' )
+# cmake, for 14.04 the version is too low from apt
 if [ "$osversion" -eq "1404" ]; then
     cd ~/Downloads
     wget https://cmake.org/files/v3.5/cmake-3.5.2.tar.gz
@@ -67,7 +80,6 @@ tar xvzf ceres-solver-1.10.0.tar.gz
 cd ceres-solver-1.10.0
 mkdir build
 cd build
-#cmake .. -DCMAKE_C_FLAGS=-fPIC -DCMAKE_CXX_FLAGS=-fPIC -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF
 cmake .. -DCMAKE_C_FLAGS="-fPIC -Wno-maybe-uninitialized" -DCMAKE_CXX_FLAGS="-fPIC -Wno-maybe-uninitialized" -DBUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF
 make -j
 sudo make install
